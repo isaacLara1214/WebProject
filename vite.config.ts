c13986a70854bc5aed523f62ba5484d65d80000b
Isaac Lara <isaac.lara014@gmail.com>
2026-04-16 15:39:26 -0400

    set up github actions for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,26 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+const getBasePath = () => {
+  if (!process.env.GITHUB_ACTIONS) {
+    return '/'
+  }
+
+  const repository = process.env.GITHUB_REPOSITORY?.split('/')[1]
+  if (!repository || repository.endsWith('.github.io')) {
+    return '/'
+  }
+
+  return `/${repository}/`
+}
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: getBasePath(),
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
